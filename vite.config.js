4db8e35a51b99412a5c08728b376e155a7a8d677
bhanu917 <bhanuprakashu1@gmail.com>
2026-05-26 12:59:50 +0530

    fix github pages path

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: "/ecommers-react/",
+  base: "/Ecommers-react/",
   plugins: [react()],
 })
