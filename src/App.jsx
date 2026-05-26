04bcfcf65cfda05d4730f2eed3e15ec2e53b6fa4
bhanu917 <bhanuprakashu1@gmail.com>
2026-05-26 13:34:53 +0530

    fix react router basename

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,6 @@ import React, { useState, useEffect } from "react";
 import Header from "./Header";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
-// import "react-toastify/dist/ReactToastify.css";
-// import 'bootstrap/dist/css/bootstrap.min.css';
 
 import "./App.css";
 import Home from "./sec-links/Home";
@@ -12,32 +10,26 @@ import Login from "./third-links/Login";
 import Register from "./third-links/Register";
 import Cart from "./sec-links/Cart";
 
-
-
 export default function App() {
   const [data, sdata] = useState([]);
+
   useEffect(() => {
-    fetch('https://fakestoreapi.com/products/')
-      .then(res => res.json())
+    fetch("https://fakestoreapi.com/products/")
+      .then((res) => res.json())
       .then((d) => sdata(d));
-
-
-
   }, []);
-  return (
-    <>
 
-      <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home data={data} />} />
-          <Route path="/Products" element={<Products data={data} />} />
-          <Route path="/Login" element={<Login />} />
-          <Route path="/Register" element={<Register />} />
-          <Route path="/Cart" element={<Cart data={data} />} />
-        </Routes>
-        <ToastContainer />
-      </BrowserRouter>
-    </>
-  )
+  return (
+    <BrowserRouter basename="/ecommers-react">
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home data={data} />} />
+        <Route path="/Products" element={<Products data={data} />} />
+        <Route path="/Login" element={<Login />} />
+        <Route path="/Register" element={<Register />} />
+        <Route path="/Cart" element={<Cart data={data} />} />
+      </Routes>
+      <ToastContainer />
+    </BrowserRouter>
+  );
 }
